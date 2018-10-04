09eda999ad6f7504fdc59c3a6e5455763e4fd34b
Talisson Junior <juniortalisson16@gmail.com>
2018-10-03 21:52:17 -0300

    UPDATE

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,10 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api/wibase/console/'
+  apiUrl: 'http://localhost:3000/console',
+  keys: {
+    storage: 'wibase-console'
+  }
 };
 
 /*
